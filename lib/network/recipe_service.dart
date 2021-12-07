ea1a0cbf91f2894007abdb3830603b6e3ca3ec29
efren sandoval <efren.sandoval@epn.edu.ec>
2021-12-07 09:32:05 -0500

    preparing the recipe service

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -1,25 +1,8 @@
-import 'package:http/http.dart';
+import 'package:chopper/chopper.dart';
+import 'recipe_model.dart';
+import 'model_response.dart';
+import 'model_converter.dart';
 
 const String apiKey = "532100fd86a91ee9b769d28466ca5352";
 const String apiId = "be0e2973";
-const String apiUrl = "https://api.edamam.com/search";
-
-class RecipeService {
-  Future getData(String url) async {
-    print("Calling url: $url");
-    final response = await get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      print(response.statusCode);
-    }
-  }
-
-  Future<dynamic> getRecipes(String query, int from, int to) async {
-    final recipeData = await getData(
-        "$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to");
-    return recipeData;
-  }
-
-  
-}
+const String apiUrl = "https://api.edamam.com";
