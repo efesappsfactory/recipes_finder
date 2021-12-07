67e11002ea2ba62d0a23cbe0b50ff3a7d586b4b0
efren sandoval <efren.sandoval@epn.edu.ec>
2021-12-07 12:42:55 -0500

    setting up the chopper client

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -6,3 +6,13 @@ import 'model_converter.dart';
 const String apiKey = "532100fd86a91ee9b769d28466ca5352";
 const String apiId = "be0e2973";
 const String apiUrl = "https://api.edamam.com";
+
+@ChopperApi()
+abstract class RecipeService extends ChopperService {
+  @Get(path: "search")
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+    @Query("q") String query,
+    @Query("from") int from,
+    @Query("to") int to,
+  );
+}
