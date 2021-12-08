3e4ca27edae2962ec8e3f853a942dd6645d41b1a
efren sandoval <efren.sandoval@epn.edu.ec>
2021-12-08 14:06:01 -0500

    generating the chopper file

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -2,6 +2,7 @@ import 'package:chopper/chopper.dart';
 import 'recipe_model.dart';
 import 'model_response.dart';
 import 'model_converter.dart';
+part 'recipe_service.chopper.dart';
 
 const String apiKey = "532100fd86a91ee9b769d28466ca5352";
 const String apiId = "be0e2973";
