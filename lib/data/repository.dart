f90ce99561fcb68bd9285ee7c214a595e221625c
efren sandoval <efren.sandoval@epn.edu.ec>
2021-12-14 14:18:16 -0500

    finding recipes and ingredients

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -1,5 +1,8 @@
 import 'models/models.dart';
 
 abstract class Repository {
-  
+  List<Recipe> findAllRecipes();
+  Recipe findRecipeById(int id);
+  List<Ingredient> findAllIngredients();
+  List<Ingredient> findRecipeIngredients(int recipeId);
 }
