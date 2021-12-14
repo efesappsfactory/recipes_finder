a6ac3ade01876fbabf0975a1fcfe63378481c108
efren sandoval <efren.sandoval@epn.edu.ec>
2021-12-14 15:44:31 -0500

    defining the memory repository

--- lib/data/memory_repository.dart
+++ lib/data/memory_repository.dart
@@ -4,4 +4,13 @@ import 'package:flutter/foundation.dart';
 import 'repository.dart';
 import 'models/models.dart';
 
+class MemoryRepository extends Repository with ChangeNotifier {
+  final List<Recipe> _currentRecipes = <Recipe>[];
+  final List<Ingredient> _currentIngredients = <Ingredient>[];
 
+  @override
+  Future init() => Future.value(null);
+
+  @override
+  void close() {}
+}
